df74eb76fdb11b996dfa1b486454ebb70b9928e8
Aleksandr Shevchenko <alexandre.shevchenko@gmail.com>
2015-05-10 18:07:48 +0500

    Добавил случайно удаленный файл

---
+++ RtbViewer/RtbViewer/TypeScript/board.ts
@@ -0,0 +1,31 @@
+﻿interface IBoard {
+	widgets: IWidget[];
+	startPosition: IPosition;
+}
+
+interface IWidget {
+	text?: string;
+	x: number;
+	y: number;
+	scale: number;
+	"type": number;
+	style: IStyle;
+	idStr: string;
+	angle: number;
+	url?: string;
+}
+
+interface IStyle {
+	ta?: string;
+	bc?: number;
+}
+
+interface IPosition {
+	a: IPoint;
+	b: IPoint;
+}
+
+interface IPoint {
+	x: number;
+	y: number;
+}
