c1e7c045413abb65dfacad73aba12e9e9843b09c
sheff146 <alexandre.shevchenko@gmail.com>
2015-05-13 22:03:54 +0500

    Реализовал отрисовку фона стикеров

--- RtbViewer/RtbViewer/TypeScript/Rendering/canvas/stickercanvasrenderer.ts
+++ RtbViewer/RtbViewer/TypeScript/Rendering/canvas/stickercanvasrenderer.ts
@@ -3,7 +3,27 @@
 		return 5;
 	}
 
-	public render(widget: IWidget, layoutCanvas: HTMLCanvasElement, viewportParams: IViewPortParams): void {
+	public render(widget: IWidget, canvas: HTMLCanvasElement, viewportParams: IViewPortParams): void {
+		var layout = LayoutHelper.countWidgetLayout(widget, viewportParams);
+		var stickerImage = new Image();
+		var realSize = { width: 223, height: 235 };
+		var screenSize = RenderHelper.countScreenSize(realSize, viewportParams, widget.scale);
 
+		stickerImage.onload = () => {
+			// ReSharper disable once RedundantTypeCast
+			var context = <CanvasRenderingContext2D>canvas.getContext("2d");
+
+			layout.width = screenSize.width;
+			layout.height = screenSize.height;
+			var x = layout.x - layout.width / 2;
+			var y = layout.y - layout.height / 2;
+
+			context.save();
+			CanvasWidgetHelper.prepareContext(context, layout);
+			context.drawImage(stickerImage, x, y, layout.width, layout.height);
+			context.restore();
+		};
+
+		stickerImage.src = "assets/sticker.png";
 	}
 }
