b8c8ac178a8ef457701f0f1de3f5fbaf51000c27
sheff146 <alexandre.shevchenko@gmail.com>
2015-05-20 09:21:13 +0500

    Поправил баг с мелькающими изображениями при перерисовке на canvas

--- RtbViewer/RtbViewer/TypeScript/Rendering/canvas/stickercanvasrenderer.ts
+++ RtbViewer/RtbViewer/TypeScript/Rendering/canvas/stickercanvasrenderer.ts
@@ -1,5 +1,6 @@
 ﻿class StickerCanvasRenderer implements ICanvasWidgetRenderer {
 	private static stickerSize = { width: 223, height: 235 };
+	private static stickerImage: HTMLImageElement;
 
 	public getWidgetType(): number {
 		return 5;
@@ -7,25 +8,33 @@
 
 	public render(widget: IWidget, context: CanvasRenderingContext2D, viewportParams: IViewPortParams): void {
 		var layout = LayoutHelper.countWidgetLayout(widget, viewportParams, StickerCanvasRenderer.stickerSize);
-		var stickerImage = new Image();
+		var k = RenderHelper.countMappingScale(viewportParams);
+		layout.padding = 15 / k.ky;
+		layout.fontSize = 40 / k.ky;
+		layout.textAlign = "center";
 
-		stickerImage.onload = () => {
-			var k = RenderHelper.countMappingScale(viewportParams);
-			layout.padding = 15 / k.ky;
-			layout.fontSize = 40 / k.ky;
-			layout.textAlign = "center";
+		if (StickerCanvasRenderer.stickerImage) {
+			StickerCanvasRenderer.renderStickerImage(widget, context, layout);
+		} else {
+			StickerCanvasRenderer.stickerImage = new Image();
 
-			context.save();
-			CanvasWidgetHelper.prepareContextForBackground(context, layout);
-			context.drawImage(stickerImage, layout.x, layout.y, layout.width, layout.height);
-			context.restore();
+			StickerCanvasRenderer.stickerImage.onload = () => {
+				StickerCanvasRenderer.renderStickerImage(widget, context, layout);
+			};
 
-			context.save();
-			CanvasWidgetHelper.prepareContextForText(context, layout);
-			context.fillText(widget.text, layout.x, layout.y + layout.padding, layout.width - 2 * layout.padding);
-			context.restore();
-		};
+			StickerCanvasRenderer.stickerImage.src = "assets/sticker.png";
+		}
 
-		stickerImage.src = "assets/sticker.png";
+		context.save();
+		CanvasWidgetHelper.prepareContextForText(context, layout);
+		context.fillText(widget.text, layout.x, layout.y + layout.padding, layout.width - 2 * layout.padding);
+		context.restore();
+	}
+
+	private static renderStickerImage(widget: IWidget, context: CanvasRenderingContext2D, layout: ILayoutParams): void {
+		context.save();
+		CanvasWidgetHelper.prepareContextForBackground(context, layout);
+		context.drawImage(StickerCanvasRenderer.stickerImage, layout.x, layout.y, layout.width, layout.height);
+		context.restore();
 	}
 }
